387cea17d2ad76c5ce997d1c1bd89d06fc5a106b
abhi8127 <abhisheky06880@gmail.com>
2024-08-21 17:48:14 +0530

    sub

--- array/Main.java
+++ array/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String args[]){
         int arr[]={1,2,3,4,5};
         for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]*2);
+            System.out.print(arr[i]*2+" ");
+            System.out.println();
+            System.out.print(arr[i]-5+ " ");
+
         }
     }
 }
